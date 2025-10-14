8e8e23cf7f904600544c7426fe427fd10449f2d7
Parker Siroishka <parkersir10@hotmail.com>
2025-10-14 20:49:45 +1000

    Update index on UserRouteProgress to prevent circular dependencies and improve query performance

---
+++ StepCounter/Migrations/20251014085630_RemoveCircularDependencies.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StepCounter.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCircularDependencies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserRouteProgress_RouteId",
+                table: "UserRouteProgress");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRouteProgress_RouteId",
+                table: "UserRouteProgress",
+                column: "RouteId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserRouteProgress_RouteId",
+                table: "UserRouteProgress");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRouteProgress_RouteId",
+                table: "UserRouteProgress",
+                column: "RouteId");
+        }
+    }
+}
